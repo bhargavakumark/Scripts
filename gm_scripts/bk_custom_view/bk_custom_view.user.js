283e8702a7dbb92cf6179e549ceb273e581f5b4a
Bhargava Kancherla <bhargavakumark@gmail.com>
2012-01-08 01:19:46 +0530

    some more websites added for autofil

--- gm_scripts/bk_custom_view/bk_custom_view.user.js
+++ gm_scripts/bk_custom_view/bk_custom_view.user.js
@@ -8,6 +8,13 @@
 // @include        http://www.indianexpress.com/news/*
 // @include        https://docs.google.com/*
 // @include        https://engtools.veritas.com/*
+// @include        https://plus.google.com*
+// @include        http://www.imdb.com/*
+// @include        https://www.irctc.co.in/*
+// @include        https://hrprod.ges.symantec.com/*
+// @include        https://www.hrworkwaysindia.com/*
+// @include        https://login.salesforce.com*
+// @include        https://mail.google.com/*
 // ==/UserScript==
 if(window.location.hostname == "in.yahoo.com") {
 	$('.wrapper clearfix').remove();	//Element by class
@@ -41,3 +48,46 @@ if(window.location.hostname == "engtools.veritas.com") {
 		document.getElementsByName('propogate_comment')[0].checked = 'checked';
 	}
 }
+if(window.location.hostname == "plus.google.com") {
+	$('.a-j c-i-j-ua tg3b4c qQWXrb g2Lc3b dfrbjb').remove();
+}
+if(window.location.hostname == "www.imdb.com") {
+	$('.aux-content-widget-2').remove();
+	$('.bottom-rhs').remove();
+	$('#maindetails_sidebar_top').remove();
+	$('#maindetails_sidebar_bottom').remove();
+}
+if(window.location.hostname == "www.irctc.co.in") {
+	if(window.location.pathname == "/cgi-bin/bv60.dll/irctc/booking/planner.do" && window.location.search.match("ReturnBank*")) {
+		document.getElementsByName('stationFrom')[0].value = "pune";
+		document.getElementsByName('stationTo')[0].value = "hyb";
+		document.getElementsByName('Day')[0].value = "09";
+		document.getElementsByName('Month')[0].value = "1";
+		document.getElementsByName('Year')[0].value = "2012";
+		document.getElementsByName('quota')[0].value = "GN";
+	}
+}
+		
+if(window.location.hostname == "hrprod.ges.symantec.com") {
+	if (window.location.pathname == "/psp/hrp/" && window.location.search.match("cmd=login")) {
+		$('#userid').val("bhargava_kancharla").change();
+	}
+}
+
+if(window.location.hostname == "www.hrworkwaysindia.com") {
+	if (window.location.pathname == "/index.jsp") {
+		$('#p1').val("0085612576").change();
+	}
+}
+
+if(window.location.hostname == "login.salesforce.com") {
+	document.getElementById('username').value = 'bhargava_kancharla@symantec.com';
+	document.getElementById('rememberUn').checked = 'checked';
+}
+
+if(window.location.hostname == "mail.google.com") {
+	$(document).ready(function(){
+		$('.mq').remove();
+	});
+}
+
