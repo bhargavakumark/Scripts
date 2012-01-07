e9df57d756ad7fa89691d05878990f1587529c1a
Bhargava Kancherla <bhargavakumark@gmail.com>
2012-01-07 20:53:01 +0530

    merging etrack into common script

--- gm_scripts/bk_custom_view/bk_custom_view.user.js
+++ gm_scripts/bk_custom_view/bk_custom_view.user.js
@@ -7,11 +7,12 @@
 // @include        http://indianexpress.com/news/*
 // @include        http://www.indianexpress.com/news/*
 // @include        https://docs.google.com/*
+// @include        https://engtools.veritas.com/*
 // ==/UserScript==
 if(window.location.hostname == "in.yahoo.com") {
-	$('.wrapper clearfix').remove();
-	$('.advertise ').remove();
-	$('#y-col2').remove();
+	$('.wrapper clearfix').remove();	//Element by class
+	$('.advertise ').remove();		
+	$('#y-col2').remove();			//Element by id
 }
 if(window.location.hostname == "indianexpress.com" || window.location.hostname == "www.indianexpress.com") {
 	var cur_location = window.location.toString();
@@ -23,6 +24,20 @@ if(window.location.hostname == "indianexpress.com" || window.location.hostname =
 	$('.picture_gal').remove();
 }
 if(window.location.hostname == "docs.google.com") {
-	$('.GDS_pageButter').remove();
 	$('#GDS_pageButter').remove();
+}
+if(window.location.hostname == "engtools.veritas.com") {
+	//document.getElementsByName('ET_state')[0].onchange = undefined;
+	//$('select[name="ET_state"]').removeAttr("onchange").bind("change", function(){ foo_2(); });
+	//$('select[name*="ET_state"]').hide();
+	if(window.location.pathname == "/Etrack/create_incident.php") {
+		document.getElementsByName('ET_description')[0].value = "";
+		document.getElementsByName('ET_user_defined_list2')[0].value = '';
+		document.getElementsByName('ET_assigned_to')[0].value = 'bkancher';
+		document.getElementsByName('ET_target_version')[0].value = '6.0';
+		document.getElementsByName('ET_build')[0].value = 'NA';
+	}
+	if(window.location.pathname == "/Etrack/modify_incident.php") {
+		document.getElementsByName('propogate_comment')[0].checked = 'checked';
+	}
 }
