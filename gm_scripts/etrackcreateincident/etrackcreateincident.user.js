a813e213345d1328c3fc1ad77e82dce95fea2c92
bhargavakumark <bhargavakumark@gmail.com>
2012-05-14 16:49:18 +0530

    changing the default version for etrack creation to 5.7MP1 removing content from etrackcreateincident as it is now done by bk_custom_view

--- gm_scripts/etrackcreateincident/etrackcreateincident.user.js
+++ gm_scripts/etrackcreateincident/etrackcreateincident.user.js
@@ -4,11 +4,3 @@
 // @description    EtrackCreateIncident
 // @include        https://engtools.veritas.com/Etrack/create_incident.php
 // ==/UserScript==
-var element = document.getElementsByName('user_defined_list2')
-element[0].value = ''
-var element = document.getElementsByName('assigned_to')
-element[0].value = 'bkancher'
-var element = document.getElementsByName('target_version')
-element[0].value = '6.0'
-var element = document.getElementsByName('description')
-element[0].value = ''
