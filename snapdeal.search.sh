817a521ed2ccc2a8d17f5296f92276f06dfd3db4
bhargavakumark <bhargavakumark@gmail.com>
2018-10-19 10:46:50 +0530

    removing rbl

--- snapdeal.search.sh
+++ snapdeal.search.sh
@@ -12,7 +12,7 @@ function searchWoohoo()
 {
     wget -O - https://www.snapdeal.com/product/woohoo-egift-card/641543859318 > /tmp/snapdeal.woohoo 2> /dev/null 
     cat /tmp/snapdeal.woohoo | grep  'has been sold out' && return
-    cat /tmp/snapdeal.woohoo | grep -A1 genericOfferClass | grep -iE 'standard|hdfc|axis|sbi|icici|rbl' | grep -E '10%|15%'
+    cat /tmp/snapdeal.woohoo | grep -A1 genericOfferClass | grep -iE 'standard|hdfc|axis|sbi|icici' | grep -E '10%|15%'
     if [ $? -eq 0 ]; then
         date
         alertmac "Found woohoo giftcards on snapdeal"
