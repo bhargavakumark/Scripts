1f6131132334fc35cb3c1d0abf1ac81cfdd48bc5
bhargavakumark <bhargavakumark@gmail.com>
2018-11-09 15:31:00 +0530

    send notification using tvwiz

--- snapdeal.search.sh
+++ snapdeal.search.sh
@@ -11,11 +11,11 @@ function usage
 function searchWoohoo()
 {
     wget -O - https://www.snapdeal.com/product/woohoo-egift-card/641543859318 > /tmp/snapdeal.woohoo 2> /dev/null 
-    cat /tmp/snapdeal.woohoo | grep  'has been sold out' && return
+    cat /tmp/snapdeal.woohoo | grep  'has been sold out' > /dev/null && return
     cat /tmp/snapdeal.woohoo | grep -A1 genericOfferClass | grep -iE 'standard|hdfc|axis|sbi|icici' | grep -E '10%|15%'
     if [ $? -eq 0 ]; then
         date
-        alertmac "Found woohoo giftcards on snapdeal"
+	/root/node/node-v5.6.0-linux-x64/bin/node /root/scraper/notification.cmd.js custom 25722518 'Snapdeal woohoo' 'Snapdeal woohoo available at discount'
         exit 0
     fi
 }
